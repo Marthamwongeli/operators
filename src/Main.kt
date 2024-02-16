f90596015029d5ba5d4ef8e98752871569a8f27f
Marthamwongeli <marthamutuaa@gmail.com>
2024-02-16 21:13:40 +0300

    add file

---
+++ src/Main.kt
@@ -0,0 +1,72 @@
+fun main(){
+    var num1= 64
+    var num2= 35
+    var result= num1 + num2
+    println(result)
+    var firstName="Jack "
+    var lastName= "Black"
+
+    var fullName=firstName + lastName
+    println(fullName)
+    num1=64
+    num2 =35
+
+    result = num1 - num2
+    println(result)
+    num1= 64
+    num2= 35
+
+    result =num1 * num2
+    println(result)
+    num1= 64
+    num2= 35
+
+    result=num1 / num2
+    println(result)
+    result=num1+num2
+    result=num1*num2
+    println(result)
+
+    num1=8
+    num2=5
+    result= num1%num2
+    println(result)
+
+     var a=8
+     var b =5
+     a = a+b
+    println(a)
+
+    a=8
+    b=5
+    a=a*b
+    a*=b
+    println(a)
+
+    var c=8
+    ++c
+    println(c)
+
+    a=8
+    b=5
+    a=
+    println(a!=b)
+
+    if (a!=b) {
+
+    }
+    else
+        
+
+
+
+
+
+
+
+
+
+
+
+
+}
